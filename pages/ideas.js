da6a1b76f06cab06dc7daed3eb76f0a5f98fd50a
Wijay000 <vraju.inbox@gmail.com>
2025-10-27 08:57:28 +0000

    fix(ideas): update video thumbnails to use hqdefault for videos without maxresdefault

--- pages/ideas.js
+++ pages/ideas.js
@@ -29,19 +29,19 @@ export default function Ideas() {
     {
       title: "Davos: Adi Godrej",
       url: "https://www.youtube.com/watch?v=6Cv_HcEdhLI",
-      thumbnail: "https://img.youtube.com/vi/6Cv_HcEdhLI/maxresdefault.jpg",
+      thumbnail: "https://img.youtube.com/vi/6Cv_HcEdhLI/hqdefault.jpg",
       type: "video"
     },
     {
       title: "Davos: Prof. Haussman",
       url: "https://www.youtube.com/watch?v=gg05B3_soH0",
-      thumbnail: "https://img.youtube.com/vi/gg05B3_soH0/maxresdefault.jpg",
+      thumbnail: "https://img.youtube.com/vi/gg05B3_soH0/hqdefault.jpg",
       type: "video"
     },
     {
       title: "WEF: Global Shapers ACM 2016",
       url: "https://www.youtube.com/results?search_query=ACM+Plenary+vijay",
-      thumbnail: "https://img.youtube.com/vi/default/maxresdefault.jpg",
+      thumbnail: "https://img.youtube.com/vi/default/hqdefault.jpg",
       type: "video"
     }
   ];
